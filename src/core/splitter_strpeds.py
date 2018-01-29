c96eebf08ca8144700eade1bbce22ba49f35a5da
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-01-29 13:55:02 +0100

    some bugs fixed

--- src/core/splitter_strpeds.py
+++ src/core/splitter_strpeds.py
@@ -18,7 +18,7 @@ class Splitter_STRPEDS(Splitter_DBS):
         self.bad_peers = []
         self.trusted_peers_discovered = []
         self.complaints = {}
-        self.p_mpl = 1
+        self.p_mpl = 0.25
         self.treshold_attack = 0.1
         self.p_tpl = 0
 
@@ -106,18 +106,19 @@ class Splitter_STRPEDS(Splitter_DBS):
         print("Bad Peers", self.bad_peers)
         if (len(self.bad_peers)/len(self.peer_list)) > self.treshold_attack:
             for b in self.bad_peers:
-                r = random.randint(0, 1)
+                r = random.uniform(0, 1)
+                print("Peer", b,"Rand", r)
                 if r <= self.p_mpl:
                     #--- Only for simulation purposes ---
                     if b in self.peer_list:
                         self.number_of_malicious -= 1
                     #------------------------------------
                     self.punish_peer(b, "by trusted")
-                    self.bad_peers.remove(b)
+                    #self.bad_peers.remove(b)
 
     def punish_TPs(self):
         for tp in self.trusted_peers_discovered:
-            r = random.randint(0,1)
+            r = random.uniform(0, 1)
             if r <= self.p_tpl:
                 self.punish_peer(tp, "by splitter")
                 self.trusted_peers_discovered.remove(tp)
