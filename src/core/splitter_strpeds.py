e76328390526c4e5fdf0b5d17a5b686b22f2ea24
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-01-31 23:29:44 +0100

    testing parameters

--- src/core/splitter_strpeds.py
+++ src/core/splitter_strpeds.py
@@ -18,7 +18,7 @@ class Splitter_STRPEDS(Splitter_DBS):
         self.bad_peers = []
         self.trusted_peers_discovered = []
         self.complaints = {}
-        self.p_mpl = 0.1
+        self.p_mpl = 0.33
         self.treshold_attack = 0.1
         self.p_tpl = 0
 
